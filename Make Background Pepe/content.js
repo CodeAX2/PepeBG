963c6b082b45f538cb8515e12897ea0d0acb3e3f
Jacob Hofer <jacobhofer1@gmail.com>
2020-02-07 13:29:12 -0700

    Improved how the images are set.
    Since some websites load images dynamically, we have to set all images every second to ensure all of them are set.

--- Make Background Pepe/content.js
+++ Make Background Pepe/content.js
@@ -1,4 +1,6 @@
-var allImageTags = document.getElementsByTagName("img");
-for (var i = 0; i < allImageTags.length; i++) {
-    allImageTags[i].src = "https://d1i4t8bqe7zgj6.cloudfront.net/09-28-2016/t_1475094050758_name_pepe.jpg";
-}
+window.setInterval(function () {
+    var allImageTags = document.getElementsByTagName("img");
+    for (var i = 0; i < allImageTags.length; i++) {
+        allImageTags[i].src = "https://d1i4t8bqe7zgj6.cloudfront.net/09-28-2016/t_1475094050758_name_pepe.jpg";
+    }
+}, 1000);

b7eec20e22f9b9124eb615cf72f2203f28db1b83
Jacob Hofer <jacobhofer1@gmail.com>
2020-02-07 13:20:46 -0700

    Made all images also use pepe

---
+++ Make Background Pepe/content.js
@@ -0,0 +1,4 @@
+var allImageTags = document.getElementsByTagName("img");
+for (var i = 0; i < allImageTags.length; i++) {
+    allImageTags[i].src = "https://d1i4t8bqe7zgj6.cloudfront.net/09-28-2016/t_1475094050758_name_pepe.jpg";
+}
